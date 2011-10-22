10dbf16ed9726c8a6f09a99636a0370b0ec11703
Yuri Dyachenko <pub.yurial@gmail.com>
2011-10-23 00:40:19 +0400

    add: process program options

--- unix_main.cpp
+++ unix_main.cpp
@@ -1,25 +1,62 @@
+#include <getopt.h>
 #include <stdlib.h>
 #include <iostream>
 
 #include "sys/cfg.h"
 #include "net/cfg.h"
 #include "sys/main.h"
+#include "version.h"
 
 static void help(std::ostream& out, int argc, char* argv[])
 {
 out << "usage: " << argv[0] << " <config file name>" << std::endl;
 }
 
+static const char options[] = "hvD";
+static const struct option long_options[] = {
+    { "help",       0,  0,  'h' },
+    { "version",    0,  0,  'v' },
+    { "daemon",     0,  0,  'D' },
+    { 0,            0,  0,   0  },
+};
+
 int main(int argc, char* argv[])
 {
-if ( 2 != argc )
+bool demonize = false;
+int option_index = 0;
+while ( true )
     {
-    help( std::cerr, argc, argv );
+    int option = getopt_long( argc, argv, options, long_options, &option_index );
+    if ( -1 == option )
+        {
+        break;
+        }
+
+    switch ( option )
+        {
+        case 'h':
+            help( std::cout, argc, argv );
+            return EXIT_SUCCESS;
+        case 'v':
+            version( std::cout );
+            return EXIT_SUCCESS;
+        case 'D':
+            demonize = true;
+            break;
+        default:
+            help( std::cerr, argc, argv );
+            return EXIT_FAILURE;
+        }
+    }
+
+if ( optind == argc )
+    {
+    std::cerr << "set config file name" << std::endl;
     return EXIT_FAILURE;
     }
 
 pugi::xml_document cfgfile;
-pugi::xml_parse_result result = cfgfile.load_file( argv[1] );
+pugi::xml_parse_result result = cfgfile.load_file( argv[optind] );
 pugi::xml_node cfg = cfgfile.first_child();
 if ( !result || !cfg )
     {
